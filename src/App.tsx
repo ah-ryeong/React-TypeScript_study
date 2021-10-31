b2cefb7928f85f99db3e39486aa4ca0656534de3
ah-ryeong <aryeong22@hanmail.net>
2021-10-31 19:38:40 +0900

    useState, 이벤트

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
+import Counter from './Counter';
 import Greetings from './Greetings';
+import MyForm from './MyForm';
 
 function App() {
   const onClick = (name: string) => {
     console.log(name);
   }
 
+  const onSubmit = (form: {name: string; description: string}) => {
+    console.log(form);
+  }
+
   return (
-    <Greetings name="리액트" onClick={onClick}/>
+    <>
+      <Greetings name="리액트" onClick={onClick}/>
+      <Counter />
+      <MyForm onSubmit={onSubmit} />
+    </>
   );
 }
 
